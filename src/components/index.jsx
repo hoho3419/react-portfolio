abc46da77d0a07fce46bfeb9d45b4efb0b8f02cd
hoho3419 <106577276+hoho3419@users.noreply.github.com>
2023-07-22 23:25:26 +0900

    Import 구문 최적화

---
+++ src/components/index.jsx
@@ -0,0 +1,5 @@
+export { default as Header } from './Header';
+export { default as Left } from './Home/Left';
+export { default as Rigth } from './Home/Rigth';
+export { default as AboutPage } from './About';
+export { default as ProjectPage } from './Project';
